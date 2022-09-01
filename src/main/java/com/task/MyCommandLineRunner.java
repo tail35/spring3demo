43ecf7ed3b15446a9b609997b4373a6860aaf8cb
tail35 <ragingfire35@gmail.com>
2022-09-01 10:56:18 +0800

    new key

--- src/main/java/com/task/MyCommandLineRunner.java
+++ src/main/java/com/task/MyCommandLineRunner.java
@@ -1,6 +1,7 @@
 package com.task;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -8,16 +9,18 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 //reference url: https://www.cnblogs.com/chenpi/p/9696310.html
 //need @ComponentScan("com.task") and @EnableScheduling at application first statement.
+@Slf4j  //use it with lombok,dno't need statement a logger.
 @Component
 @Order(value = 2) //if multip CommandLineRunner object value is execute order.
 public class MyCommandLineRunner implements CommandLineRunner {
 
-    private final static Logger logger = LoggerFactory.getLogger(MyCommandLineRunner.class);
+    //private final static Logger logger = LoggerFactory.getLogger(MyCommandLineRunner.class);
     @Override//only execute once after start app.
     public void run(String... var1) throws Exception{
-        System.out.println("This will be execute when the project was started!");
+        //System.out.println("This will be execute when the project was started!");
         int i=0;
         i++;
-        logger.debug("11111");
+        //logger.debug("11111");
+        log.debug("11111");
     }
 }
